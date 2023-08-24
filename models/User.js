d78722250f343a7f6be024e397b728d1353411ab
Brandon Mountan <brandonmountan1@gmail.com>
2023-08-23 20:14:34 -0500

    initial commit laptop

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        
+    },
+    age: Number,
+    applications: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Application',
+      },
+    ],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
